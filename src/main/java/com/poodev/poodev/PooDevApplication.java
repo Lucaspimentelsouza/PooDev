e5c8dd6c55605e43d1768a2a856683f367ce2ef9
Lucas <lucasccuenf2020@gmail.com>
2026-09-10 10:16:12 -0300

    First Commit

---
+++ src/main/java/com/poodev/poodev/PooDevApplication.java
@@ -0,0 +1,13 @@
+package com.poodev.poodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PooDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poodev/poodev/PooDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poodev.poodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
